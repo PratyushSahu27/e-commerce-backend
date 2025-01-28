b01a03dc7f0cd3bdd322487c06278009b5accb3c
Pratyush Sahu <68014145+PratyushSahu27@users.noreply.github.com>
2025-01-28 23:53:46 +0530

    Update getUserOrBranch response and added try catch

--- app/controllers/GetUserOrBranchController.ts
+++ app/controllers/GetUserOrBranchController.ts
@@ -1,15 +1,42 @@
 import { Request, Response } from "express";
-import { Branch, Users } from "../DB/models/Models.js";
+import { Branch, Users } from "../DB/models/models.js";
 
 export const getUserOrBranch = async (request: Request, response: Response) => {
   console.log("Get user or branch");
-  if (request.body.user) {
-    let userData = await Users.findOne({ smId: request.body.user.id });
-    response.json({ userData, state: "User" });
-  } else if (request.body.branch) {
-    let userData = await Branch.findOne({
-      branch_id: request.body.branch.branch_id,
+  try {
+    if (request.body.user) {
+      let userData = await Users.findOne(
+        { smId: request.body.user.id },
+        {
+          _id: 0,
+          smId: 1,
+          name: 1,
+          phoneNumber: 1,
+          addresses: 1,
+          guideId: 1,
+          email: 1,
+          total_pv: 1,
+        }
+      );
+      response.json({ userData, state: "User" });
+    } else if (request.body.branch) {
+      let userData = await Branch.findOne(
+        {
+          branch_id: request.body.branch.branch_id,
+        },
+        {
+          _id: 0,
+          branch_name: 1,
+          address: 1,
+          email_address: 1,
+        }
+      );
+      response.json({ userData, state: "Branch" });
+    }
+  } catch {
+    return response.status(400).json({
+      success: false,
+      errors: `Failed to get ${request.body.user ? "user" : "branch"}`,
     });
-    response.json({ userData, state: "Branch" });
   }
 };
